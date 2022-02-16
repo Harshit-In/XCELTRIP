5c425a59ab78280896991df3b4bc0a72cd6b78a6
karunendumishra <68456371+karunendumishra@users.noreply.github.com>
2022-02-16 12:27:41 +0530

    commit

--- XCELTRIP/client/src/routes/router.js
+++ XCELTRIP/client/src/routes/router.js
@@ -1,5 +1,11 @@
 import React, { useEffect } from "react";
-import { Route, Switch, useLocation, Redirect, NavLink } from "react-router-dom";
+import {
+  Route,
+  Switch,
+  useLocation,
+  Redirect,
+  NavLink,
+} from "react-router-dom";
 import { useSelector } from "react-redux";
 import Dashboard from "./Dashboard";
 import Topbar from "../components/dashboard/Topbar";
@@ -101,48 +107,50 @@ export default function Router(props) {
             {/* <Route exact path="/" component={SignUpForm} />
             <Route path="/sign-in" component={SignInForm} /> */}
 
-            <div className="appAside" />
-          <div className="appForm">
-            <div className="pageSwitcher">
-              <NavLink
-                to="/sign-in"
-                activeClassName="pageSwitcherItem-active"
-                className="pageSwitcherItem"
-              >
-                Sign In
-              </NavLink>
-              <NavLink
-                exact
-                to="/"
-                activeClassName="pageSwitcherItem-active"
-                className="pageSwitcherItem"
-              >
-                Sign Up
-              </NavLink>
-            </div>
+            <div className="App">
+              <div className="appAside" />
+              <div className="appForm">
+                <div className="pageSwitcher">
+                  <NavLink
+                    to="/sign-in"
+                    activeClassName="pageSwitcherItem-active"
+                    className="pageSwitcherItem"
+                  >
+                    Sign In
+                  </NavLink>
+                  <NavLink
+                    exact
+                    to="/"
+                    activeClassName="pageSwitcherItem-active"
+                    className="pageSwitcherItem"
+                  >
+                    Sign Up
+                  </NavLink>
+                </div>
 
-            <div className="formTitle">
-              <NavLink
-                to="/sign-in"
-                activeClassName="formTitleLink-active"
-                className="formTitleLink"
-              >
-                Sign In
-              </NavLink>{" "}
-              or{" "}
-              <NavLink
-                exact
-                to="/"
-                activeClassName="formTitleLink-active"
-                className="formTitleLink"
-              >
-                Sign Up
-              </NavLink>
-            </div>
+                <div className="formTitle">
+                  <NavLink
+                    to="/sign-in"
+                    activeClassName="formTitleLink-active"
+                    className="formTitleLink"
+                  >
+                    Sign In
+                  </NavLink>{" "}
+                  or{" "}
+                  <NavLink
+                    exact
+                    to="/"
+                    activeClassName="formTitleLink-active"
+                    className="formTitleLink"
+                  >
+                    Sign Up
+                  </NavLink>
+                </div>
 
-            <Route exact path="/" component={SignUpForm} />
-            <Route path="/sign-in" component={SignInForm} />
-          </div>
+                <Route exact path="/" component={SignUpForm} />
+                <Route path="/sign-in" component={SignInForm} />
+              </div>
+            </div>
           </div>
         </div>
       </Switch>
