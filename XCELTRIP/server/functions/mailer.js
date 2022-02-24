2e6bfe80a94d91cd0e3bde09e60ce87da5b1b072
Harshit-In <thorbond2020@gmail.com>
2022-02-24 13:31:19 +0530

    mailer

--- XCELTRIP/server/functions/mailer.js
+++ XCELTRIP/server/functions/mailer.js
@@ -75,7 +75,7 @@ function generateOtpHTML(otp, info ) {
           html = `<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
                   <div style="margin:50px auto;width:70%;padding:20px 0">
                       <div style="border-bottom:1px solid #eee">
-                         <h5>${info&&info.website?info.website:'#'}" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600"></h5>
+                         <h5 style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">${info&&info.website?info.website:'GlobleDiffi'}</h5>
                       </div>
                       <p style="font-size:1.1em">Hi,</p>
                       <p>Thank you for choosing Globle DIFFI Pool. Use the following OTP to complete your Sign Up procedures. OTP is valid for 5 minutes</p>
