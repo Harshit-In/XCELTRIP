92914c078c2c097318aab914ecd83b01e79dd2ba
Harshit-In <thorbond2020@gmail.com>
2022-03-15 15:37:38 +0530

    admin

--- XCELTRIP/server/models/admin.js
+++ XCELTRIP/server/models/admin.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 const admin = new mongoose.Schema({
     admin_name: { type: String },
+    owner_address: { type: String },
     email: { type: String },
     hash_password: { type: String, required: true },
     admin_wallet: { type: Number, default: 0.00},
     repurchase_wallet: { type: Number, default: 0.00},
+
 }, {timestamps: true, collection:'Admin'})
 
 
