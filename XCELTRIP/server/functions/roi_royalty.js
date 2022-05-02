8e67dc03e799e2fbc67a2074ef197e253f08f78a
karunendumishra <68456371+karunendumishra@users.noreply.github.com>
2022-05-02 17:47:32 +0530

    commit

--- XCELTRIP/server/functions/roi_royalty.js
+++ XCELTRIP/server/functions/roi_royalty.js
@@ -76,54 +76,83 @@ async function  resetCashbackAmount() {
 };
 
 async function sendMonthlyRoyalty(dt = null) {
-  const UserModel = require("./models/user");
-  const HistoryModel = require("./models/history");
-  const tDate = dt ? new Date(dt) : new Date();
-  /* tDate.setUTCHours(0, 0, 0, 0);
-      const sDate = new Date(new Date(
-        new Date().getMonth() + "-" + 1 + "-" + new Date().getFullYear()
-      ).getTime() + (3600 * 24 * 1000));
-      sDate.setUTCHours(0, 0, 0, 0); */
-  console.log(tDate);
-  const r = await HistoryModel.aggregate([
-    {
-      $match: {
-        income_type: "ID Activation",
-        createdAt: { $gt: tDate },
+    const UserModel = require("../models/user");
+    const HistoryModel = require("../models/history");
+    const moment = require("moment");
+    const tDate = dt ? moment(dt) : moment();
+    /* tDate.setUTCHours(0, 0, 0, 0);
+    const sDate = new Date(new Date(
+      new Date().getMonth() + "-" + 1 + "-" + new Date().getFullYear()
+    ).getTime() + (3600 * 24 * 1000));
+    sDate.setUTCHours(0, 0, 0, 0); */
+    console.log(tDate);
+    const r = await HistoryModel.aggregate([
+      {
+        $match: {
+          income_type: "ID Activation",
+          //createdAt: { $gt: tDate },
+        },
       },
-    },
-    {
-      $group: {
-        _id: { $dateToString: { format: "%Y-%m", date: "$createdAt" } },
-        totalBusiness: { $sum: "$amount" },
-        count: { $sum: 1 },
+      {
+        $group: {
+          _id: { $dateToString: { format: "%Y-%m", date: "$createdAt" } },
+          totalBusiness: { $sum: "$amount" },
+          count: { $sum: 1 },
+        },
       },
-    },
-  ]).sort({ _id: -1 });
-  const lastMonthBusiness = r[0].totalBusiness;
-  const royaltyAmount = (lastMonthBusiness * 5) / 100;
-  console.log(
-    "totalBusiness ",
-    lastMonthBusiness,
-    " royaltyAmount ",
-    royaltyAmount
-  );
-  const level4Members = await UserModel.find({ level: 4 }).count();
-  const level5Members = await UserModel.find({ level: 5 }).count();
-  const level4rAmount = royaltyAmount / level4Members;
-  const level5rAmount = royaltyAmount / level5Members;
-  console.log(
-    "level4Members : ",
-    level4Members,
-    "level5Members : ",
-    level5Members,
-    "level4rAmount : ",
-    level4rAmount,
-    "level5rAmount : ",
-    level5rAmount
-  );
-  console.log("r", r);
-};
+      { $sort: { _id: -1 } },
+    ]);
+    console.log(r);
+    const lastMonthBusiness = r[0]?.totalBusiness;
+    const royaltyAmount = (lastMonthBusiness * 5) / 100;
+    console.log(
+      "totalBusiness ",
+      lastMonthBusiness,
+      " royaltyAmount ",
+      royaltyAmount
+    );
+    const level4Members = await UserModel.find({ level: 4 });
+    const level5Members = await UserModel.find({ level: 5 });
+    const level4rAmount = royaltyAmount / level4Members.length;
+    const level5rAmount = royaltyAmount / level5Members.length;
+    console.log(
+      "level4Members : ",
+      level4Members.length,
+      "level5Members : ",
+      level5Members.length,
+      "level4rAmount : ",
+      level4rAmount,
+      "level5rAmount : ",
+      level5rAmount,
+    );
+    console.log("r", r);
+    const RoyaltyHistoryModel = require("../models/royalty");
+    level4Members.map((member)=>{
+      /* await UserModel.updateOne(
+      { member_id: member.member_id },
+      { $inc: { cashback_wallet: level4rAmount/2 } }
+    );
+    await RoyaltyHistoryModel.updateOne({ member_id: member.member_id,},{
+      $set: {
+        date: new Date(),
+      },
+      $inc: {royalty_amount: level4rAmount}
+    }); */
+    })
+  
+    level5Members.map((member)=>{
+      /* await UserModel.updateOne(
+      { member_id: member.member_id },
+      { $inc: { cashback_wallet: level5rAmount/2 } }
+    );
+    await RoyaltyHistoryModel.updateOne({ member_id: member.member_id,},{
+      $set: {
+        date: new Date(),
+      },
+      $inc: {royalty_amount: level5rAmount}
+    }); */
+    })
+  }
 
 module.exports = {
     sendMonthlyROI,
